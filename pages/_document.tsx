c8ce22dadd0aca17f4ecc03a746300b4b8852d86
Daniel von Atzigen <daniel@vatz.ch>
2022-08-11 21:01:35 +0200

    Configure and a fix eslint

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+
+class AppDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?display=swap&family=Scope+One" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?display=swap&family=Encode+Sans+Expanded" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?display=swap&family=Eczar" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+export default AppDocument
