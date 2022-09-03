262d43f299887a869612d0686f6da802a34b6407
Daniel von Atzigen <daniel@vatz.ch>
2022-09-03 10:45:35 +0200

    Add `CalendarEventForm` component

---
+++ components/Calendar/Event/CalendarEventForm.tsx
@@ -0,0 +1,97 @@
+import UiLocalDateInput from '@/components/Ui/Input/UiLocalDateInput'
+import UiSelectInput from '@/components/Ui/Input/UiSelectInput'
+import UiTextInput from '@/components/Ui/Input/UiTextInput'
+import UiToggle from '@/components/Ui/Toggle/UiToggle'
+import UiGrid from '@/components/Ui/UiGrid'
+import UiSubmit from '@/components/Ui/UiSubmit'
+import LocalDate from '@/models/base/LocalDate'
+import { ModelData } from '@/models/base/Model'
+import CalendarEvent, { validateCalendarEvent } from '@/models/CalendarEvent'
+import { allGroups } from '@/models/Group'
+import { createCalendarEvent, updateCalendarEvent } from '@/store/calendar/event/calendar-event.slice'
+import { useAppDispatch } from '@/store/hooks'
+import theme from '@/theme-utils'
+import { Form, FormField, useCancel, useForm, useSubmit, useValidate } from '@daniel-va/react-form'
+import React from 'react'
+import styled from 'styled-components'
+
+interface Props {
+  event?: CalendarEvent | null
+  onClose: () => void
+}
+
+const CalendarEventForm: React.FC<Props> = ({ event = null, onClose: pushClose }) => {
+  const form = useForm<ModelData<CalendarEvent>>(event, () => ({
+    name: '',
+    startsAt: LocalDate.fromDate(new Date()),
+    endsAt: LocalDate.fromDate(new Date()),
+    groupIds: [],
+    isInternal: false,
+  }))
+  useValidate(form, validateCalendarEvent)
+
+  const dispatch = useAppDispatch()
+  useSubmit(form, async (data) => {
+    await (event === null
+      ? dispatch(createCalendarEvent(data))
+      : dispatch(updateCalendarEvent({ id: event.id, data }))
+    )
+    pushClose()
+  })
+  useCancel(form, pushClose)
+
+  return (
+    <Form state={form}>
+      <FormField field={form.name}>{(inputProps) => (
+        <UiTextInput {...inputProps} label="Name" />
+      )}</FormField>
+      <UiGrid gap={1}>
+        <UiGrid.Col>
+          <FormField field={form.groupIds}>{(inputProps) => (
+            <UiSelectInput
+              {...inputProps}
+              label="Stufen"
+              options={allGroups}
+              optionValue={({ id }) => id}
+              optionName={({ name }) => name}
+              hasMultiple
+            />
+          )}</FormField>
+        </UiGrid.Col>
+        <UiGrid.Col size="auto">
+          <FormField field={form.isInternal}>{({ value, onChange }) => (
+            <InternalLabel>
+              <div>
+                Rover-Event?
+              </div>
+              <UiToggle value={value ?? false} onChange={onChange} />
+            </InternalLabel>
+          )}</FormField>
+        </UiGrid.Col>
+      </UiGrid>
+      <UiGrid gap={1}>
+        <UiGrid.Col>
+          <FormField field={form.startsAt}>{(inputProps) => (
+            <UiLocalDateInput {...inputProps} label="Beginn" />
+          )}</FormField>
+        </UiGrid.Col>
+        <UiGrid.Col>
+          <FormField field={form.endsAt}>{(inputProps) => (
+            <UiLocalDateInput {...inputProps} label="Ende" />
+          )}</FormField>
+        </UiGrid.Col>
+      </UiGrid>
+      <UiSubmit />
+    </Form>
+  )
+}
+export default CalendarEventForm
+
+const InternalLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.spacing(1)};
+  height: calc(100% - 1rem);
+  font-size: 0.9em;
+  font-weight: bold;
+`
